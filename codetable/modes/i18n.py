302e75da9b7851013b1d0d355c4e69066a9eb6fc
0x1618 <80158535+0x1618@users.noreply.github.com>
2026-01-20 00:06:31 +0100

    refactor(i18n): use hetman-kit-localize

--- codetable/modes/i18n.py
+++ codetable/modes/i18n.py
@@ -1,25 +1,15 @@
-from contextvars import ContextVar
-from typing import ClassVar
+from hetman_kit_localize import Localize
 
 from codetable.core.code import Code
 
 
-class i18n(Code):
+class i18n(Code, Localize):
     """
     A Code implementation that supports internationalization (i18n).
 
     This class allows defining messages for different locales and retrieving the
     appropriate message based on the current context locale.
-
-    Attributes:
-        _base_locale (ClassVar[str]): The default locale to fall back to if the current
-            locale's message is not found.
-        _current_locale (ClassVar[ContextVar[str]]): A context variable holding the
-            current locale. Defaults to "codetable.locale".
     """
-    _base_locale: ClassVar[str]
-    _current_locale: ClassVar[ContextVar[str]] = ContextVar("codetable.locale")
-
     def __init__(self, **msg: str) -> None:
         """
         Initializes an i18n Code instance with messages for different locales.
@@ -39,56 +29,4 @@ class i18n(Code):
             str: The message corresponding to the current locale, or the base locale
                 if the current locale is not available.
         """
-        localized: str = self.msg.get(
-            self.get_locale(), self.msg[self.get_base_locale()]
-        )
-
-        return localized
-
-    @classmethod
-    def get_locale(cls) -> str:
-        """
-        Retrieves the current locale from the context.
-
-        Returns:
-            str: The current locale, or the base locale if no locale is set in the context.
-        """
-        return cls._current_locale.get(cls._base_locale)
-
-    @classmethod
-    def set_locale(cls, locale: str) -> None:
-        """
-        Sets the current locale for the context.
-
-        Args:
-            locale (str): The locale code to set (e.g., "en", "pl").
-        """
-        cls._current_locale.set(locale)
-
-    @classmethod
-    def get_base_locale(cls) -> str:
-        """
-        Retrieves the base locale.
-
-        Returns:
-            str: The base locale code.
-
-        Raises:
-            RuntimeError: If `_base_locale` has not been set.
-        """
-        if not hasattr(cls, "_base_locale"):
-            raise RuntimeError(
-                "Base locale is not set. Use i18n.set_base_locale() to define the base locale."
-            )
-
-        return cls._base_locale
-
-    @classmethod
-    def set_base_locale(cls, base_locale: str) -> None:
-        """
-        Sets the base locale.
-
-        Args:
-            base_locale (str): The locale code to serve as the default fallback.
-        """
-        cls._base_locale = base_locale
+        return self.resolve_translation(data=self.msg)
